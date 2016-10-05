60075439131a89a51a579900c728e9b31ee78f8d
Julien <julien-gm@users.noreply.github.com>
2016-10-05 17:15:11 +0200

    Init bootstrap (#1)

--- src/StatsBundle/Controller/DefaultController.php
+++ src/StatsBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace StatsBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -10,12 +11,10 @@ class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @Security("is_granted('IS_AUTHENTICATED_FULLY')")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        return $this->render('default/index.html.twig', ['user' => $this->getUser()]);
     }
 }
--- src/StatsBundle/Entity/User.php
+++ src/StatsBundle/Entity/User.php
@@ -21,6 +21,5 @@ class User extends BaseUser
     public function __construct()
     {
         parent::__construct();
-        // your own logic
     }
 }
